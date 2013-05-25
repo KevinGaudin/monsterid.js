2a4b748a297b0d7fce54648d63cd179daa4b764a
Kevin Gaudin <kevin.gaudin@gmail.com>
2013-05-26 01:00:25 +0200

    Some more ADN. Add watch task to build system.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            js: {
+                files: ['Gruntfile.js', 'src/*.js'],
+                tasks: ['uglify'],
+                spawn: true
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -16,6 +23,9 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-regarde');
+
+    grunt.renameTask('regarde', 'watch');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
