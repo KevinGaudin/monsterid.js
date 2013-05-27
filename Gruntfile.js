33bcdcb9b5b07361d97896b52c6d3ccfe7628b99
Kevin Gaudin <kevin.gaudin@gmail.com>
2013-05-27 22:58:44 +0200

    Add a build for a concat without minifying.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,18 @@ module.exports = function(grunt) {
                 spawn: true
             }
         },
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['src/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'build/monsterid.js'
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -23,11 +35,13 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-regarde');
 
     grunt.renameTask('regarde', 'watch');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('dist', ['concat','uglify']);
 
 };
